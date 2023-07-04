5783c715bf6204f6918f0dd0307a189bd1a5424d
“Roman <fedkoroma2@gmail.comgit config --global user.email fedkoroma2@gmail.com>
2023-07-04 13:10:31 +0300

    0.1

---
+++ src/main/java/com/example/notekeep_bot/NoteKeepBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.notekeep_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteKeepBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteKeepBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notekeep_bot/NoteKeepBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notekeep_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteKeepBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
